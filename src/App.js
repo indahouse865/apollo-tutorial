1d1b7c4d35d7e06051748df0eeddda074853d0c2
David Estrich <david.estrich280@gmail.com>
2019-10-27 15:19:19 -0400

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
+import { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'
+import { Query } from 'react-apollo'
+
+
+const client = new ApolloClient({
+  uri: 'https://api-useast.graphcms.com/v1/ck294t2s134vc01hi2di9eldf/master'
+});
+
+const testQuery = gql`
+  {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+//Run query from outside of react
+// client.query({
+//   query: testQuery
+// }).then(res => console.log(res))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>  
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a> 
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
